447ab8c7b641dd57a91e8b8cd36c5f4ab5096c88
krishnakumar.B <k4kr95@gmail.com>
2019-07-04 16:56:48 +0530

    updated finally

--- task/spiders/hashwave.py
+++ task/spiders/hashwave.py
@@ -6,14 +6,17 @@ class ToScrapeCSSSpider(scrapy.Spider):
         'https://www.globaltrade.net/United-States/expert-service-provider.html',
         "https://www.globaltrade.net"
     ]
+    companyUrl=""
 
     def parse(self, response):
         for providers in response.xpath("//p[@class='sp-name']"):
             endPart = str(providers.xpath("a/@href").extract())
             endPart = endPart[3:-2]
-            companyUrl = self.start_urls[1]+endPart
+            self.companyUrl = self.start_urls[1]+endPart
+        
 
-            yield scrapy.Request(url=companyUrl,callback=self.parseCompanyPage)
+            yield scrapy.Request(url=self.companyUrl,callback=self.parseCompanyPage)
+            
             
             nextPage=str(response.xpath('.//div[@class="nav-page"]/a[text()="Next >"]/@href').extract_first())    
             actualPage=self.start_urls[1]+nextPage
@@ -23,14 +26,16 @@ class ToScrapeCSSSpider(scrapy.Spider):
     def parseCompanyPage(self, response):
        
         yield {
-            'logoUrl': response.xpath(".//img[@class='lazy']/@src").extract_first(),
-            'title': response.xpath(".//h1/span/text()").extract_first(),
-            'subTitle': response.xpath(".//span[@class='sub']/text()").extract_first(),
-            'primaryLocation':response.xpath(".//span[@itemprop='addressLocality']/text()").extract_first(),
-            'areaOfExpertise':response.xpath(".//a[@class='mainExp']/text()").extract_first(),
-            'about':response.xpath(".//tr[td/text() ='About:']/td/p/text()").extract_first(),
-            'website':response.xpath(".//tr[td/text() ='Website:']/td/a/@href").extract_first(),
-            "languageSpoken":response.xpath(".//tr[td/text() ='Languages spoken:']/td[2]/text()").extract_first()
+             
+            'pageUrl':self.companyUrl,
+            'logoUrl': response.xpath("normalize-space(.//img[@class='lazy']/@src)").extract_first(),
+            'title': response.xpath("normalize-space(.//h1/span/text())").extract_first(),
+            'subTitle': response.xpath("normalize-space(.//span[@class='sub']/text())").extract_first(),
+            'primaryLocation':response.xpath("normalize-space(.//span[@itemprop='addressLocality']/text())").extract_first(),
+            'areaOfExpertise':response.xpath("normalize-space(.//a[@class='mainExp']/text())").extract_first(),
+            'about':response.xpath("normalize-space(.//tr[td/text() ='About:']/td/p/text())").extract_first(),
+            'website':response.xpath("normalize-space(.//tr[td/text() ='Website:']/td/a/@href)").extract_first(),
+            "languageSpoken":response.xpath("normalize-space(.//tr[td/text() ='Languages spoken:']/td[2]/text())").extract_first()
         }
 
 
